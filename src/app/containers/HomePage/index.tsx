4d9847dce75f7d0f821db3abc1c3a80651e2f565
Michael Chilaka <obichilaka@gmail.com>
2022-08-14 01:09:46 +0100

    setting up all packages

---
+++ src/app/containers/HomePage/index.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const PageContainer = styled.div`
+    ${tw`
+       flex
+       flex-col
+       w-full
+       h-full
+       items-center
+       overflow-x-hidden
+    `}
+`;
+
+export function HomePage() {
+    return <PageContainer>
+        Hello Adrian
+    </PageContainer>
+}
