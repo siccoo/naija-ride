3cdf820101b12a8eae74821be1e6b94c82d9ca8e
Michael Chilaka <obichilaka@gmail.com>
2022-08-16 16:10:50 +0100

    updates-on-navItems

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import tw from "twin.macro";
 import { Logo } from "../logo";
+import { NavItems } from "./navItems";
 
 const NavbarContainer = styled.div`
    min-height: 68px;
@@ -24,6 +25,7 @@ export function Navbar() {
       <LogoContainer>
         <Logo />
       </LogoContainer>
+      <NavItems />
     </NavbarContainer>
   )
 }
---
+++ src/app/components/navbar/navItems.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+
+
+const ListContainer = styled.ul`
+${tw`
+flex
+list-none
+`}
+`;
+
+const NavbarItems = styled.li`
+${tw`
+text-xs
+md:text-base
+text-black
+font-medium
+mr-1
+md:mr-5
+cursor-pointer
+transition
+duration-300
+ease-in-out
+hover:text-gray-700
+`};
+`;
+
+export function NavItems() {
+return(
+    <ListContainer>
+        <NavbarItems>
+            <a href="#">Home</a>
+        </NavbarItems>
+        <NavbarItems>
+            <a href="#">Cars</a>
+        </NavbarItems>
+        <NavbarItems>
+            <a href="#">About</a>
+        </NavbarItems>
+        <NavbarItems>
+            <a href="#">Contact us</a>
+        </NavbarItems>
+    </ListContainer>
+)
+}
