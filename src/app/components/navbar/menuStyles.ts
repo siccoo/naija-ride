424e74d06e020478cb139982ac4571646f9aa0cd
Michael Chilaka <obichilaka@gmail.com>
2022-08-18 01:27:52 +0100

    updates on navItems.tsx

---
+++ src/app/components/navbar/menuStyles.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    bmBurgerButton: {
+        position: "absolute",
+        width: "20px",
+        height: "20px",
+        right: "20px",
+        top: "20px "
+    }
+}
--- src/app/components/navbar/navItems.tsx
+++ src/app/components/navbar/navItems.tsx
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 import tw from "twin.macro";
 
-import {Slide as Menu} from "react-burger-menu";
+import { Slide as Menu } from "react-burger-menu";
 import { useMediaQuery } from "react-responsive";
+import { SCREENS } from "../responsive";
 
 
 const ListContainer = styled.ul`
 ${tw`
+// react-burger-menu
 flex
 list-none
 `}
@@ -29,20 +31,42 @@ hover:text-gray-700
 `;
 
 export function NavItems() {
-return(
-    <ListContainer>
-        <NavbarItems>
-            <a href="#">Home</a>
-        </NavbarItems>
-        <NavbarItems>
-            <a href="#">Cars</a>
-        </NavbarItems>
-        <NavbarItems>
-            <a href="#">About</a>
-        </NavbarItems>
-        <NavbarItems>
-            <a href="#">Contact us</a>
-        </NavbarItems>
-    </ListContainer>
-)
+
+    const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+
+    if (isMobile) return (
+        <Menu>
+            <ListContainer>
+                <NavbarItems>
+                    <a href="#">Home</a>
+                </NavbarItems>
+                <NavbarItems>
+                    <a href="#">Cars</a>
+                </NavbarItems>
+                <NavbarItems>
+                    <a href="#">About</a>
+                </NavbarItems>
+                <NavbarItems>
+                    <a href="#">Contact us</a>
+                </NavbarItems>
+            </ListContainer>
+        </Menu>
+    )
+
+    return (
+        <ListContainer>
+            <NavbarItems>
+                <a href="#">Home</a>
+            </NavbarItems>
+            <NavbarItems>
+                <a href="#">Cars</a>
+            </NavbarItems>
+            <NavbarItems>
+                <a href="#">About</a>
+            </NavbarItems>
+            <NavbarItems>
+                <a href="#">Contact us</a>
+            </NavbarItems>
+        </ListContainer>
+    )
 }
